1ee72ae262c45805e989c3dbd0cca8ed11228756
Ahmed Imran <ahmedimran.centrox@gmail.com>
2024-03-18 06:51:39 +0500

    router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
-
+import React from "react";
+import "./globals.css";
+import { Route, Routes } from "react-router-dom";
+import SignIn from "./auth/forms/SignIn";
+import { Home } from "./root/pages";
+import SignUp from "./auth/forms/SignUp";
+import AuthLayout from "./auth/forms/AuthLayout";
+import RootLayout from "./root/RootLayout";
 const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <main className="flex h-screen">
+      <Routes>
+        {/* PUNLIC ROUTES */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Route>
 
-export default App
+        {/* PRIVSTE ROUTES */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
